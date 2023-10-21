640844078d464ac74aabb024b51e3c827ea1ff24
Raja <s.rajarathinam1999@gmail.com>
2023-10-21 17:26:12 +0530

    seo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,34 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+{
+  /* <script> 
+window.dataLayer = window.dataLayer || []; 
+function gtag(){
+dataLayer.push(arguments);
+} gtag('js', new Date()); gtag('config', 'AW-11384016350'); 
+</script> */
+}
 
 export default function Document() {
   return (
     <Html lang="en">
-      
       <Head />
       <title>SK BeautyVerse</title>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=AW-11384016350"
+      ></Script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || []; 
+          function gtag(){
+          dataLayer.push(arguments);
+          } gtag('js', new Date()); gtag('config', 'AW-11384016350'); 
+          `,
+        }}
+      />
       <body>
         <Main />
         <NextScript />
