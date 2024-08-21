e1b330522d5829967082b12a90e6df6e9733bc1e
Raja <raja.rathinam@pcisage.com>
2024-08-21 18:16:40 +0530

    meta

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -31,6 +31,15 @@ export default function Document() {
         }}
       />
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MC9VM5PW"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        </noscript>
+
         <Main />
         <NextScript />
       </body>
