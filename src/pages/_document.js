2458597e3739997a09e11c73a415a7063fccc6f6
Raja <raja.rathinam@pcisage.com>
2024-08-21 18:17:42 +0530

    meta

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -36,7 +36,10 @@ export default function Document() {
             src="https://www.googletagmanager.com/ns.html?id=GTM-MC9VM5PW"
             height="0"
             width="0"
-            style="display:none;visibility:hidden"
+            style={{
+              display: "none",
+              visibility: "hidden",
+            }}
           ></iframe>
         </noscript>
 
