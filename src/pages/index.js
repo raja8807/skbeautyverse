89fb0be16fac4d7bb5a75a9b651d76151fab4129
Raja <raja.rathinam@pcisage.com>
2024-08-21 20:14:31 +0530

    meta

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,61 +10,37 @@ const Home = ({ homeData = {}, blogs, reviews }) => {
         <title>SK Beauty-Verse</title>
         <meta
           name="description"
-          content='I am SUSHMITHA KARTHIK, your dedicated makeup artist, Certified by "Lakme Academy" specializing in hair, skin, and bridal makeup. Discover personalized beauty experiences crafted with premium products, which does not cause damage to skin & hair. "Make your beautyful day, More Beutiful with our Makup Services!"'
+          content='I am SUSHMITHA KARTHIK, your dedicated makeup artist, Certified by "Lakme Academy" specializing in hair, skin, and bridal makeup. Discover personalized beauty experiences crafted with premium products, which do not cause damage to skin & hair. "Make your beautyful day, More Beutiful with our Makup Services!"'
         />
-
         <meta property="og:url" content="https://www.skbeautyverse.com" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="SK Beauty-Verse" />
         <meta
           property="og:description"
-          content='I am SUSHMITHA KARTHIK, your dedicated makeup artist, Certified by "Lakme Academy" specializing in hair, skin, and bridal makeup. Discover personalized beauty experiences crafted with premium products, which does not cause damage to skin & hair. "Make your beautyful day, More Beutiful with our Makup Services!"'
+          content='I am SUSHMITHA KARTHIK, your dedicated makeup artist, Certified by "Lakme Academy" specializing in hair, skin, and bridal makeup. Discover personalized beauty experiences crafted with premium products, which do not cause damage to skin & hair. "Make your beautyful day, More Beutiful with our Makup Services!"'
         />
         <meta
           property="og:image"
           content="https://opengraph.b-cdn.net/production/images/1171157b-250d-48d1-8b38-936992b37a2d.png?token=a2WslOTnXlU7wkxgcx0HhENUn1vM6WdBYa8CR_L9Q0U&height=949&width=1200&expires=33260249685"
         />
-
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="skbeautyverse.com" />
         <meta property="twitter:url" content="https://www.skbeautyverse.com" />
         <meta name="twitter:title" content="SK Beauty-Verse" />
         <meta
           name="twitter:description"
-          content='I am SUSHMITHA KARTHIK, your dedicated makeup artist, Certified by "Lakme Academy" specializing in hair, skin, and bridal makeup. Discover personalized beauty experiences crafted with premium products, which does not cause damage to skin & hair. "Make your beautyful day, More Beutiful with our Makup Services!"'
+          content='I am SUSHMITHA KARTHIK, your dedicated makeup artist, Certified by "Lakme Academy" specializing in hair, skin, and bridal makeup. Discover personalized beauty experiences crafted with premium products, which do not cause damage to skin & hair. "Make your beautyful day, More Beutiful with our Makup Services!"'
         />
         <meta
           name="twitter:image"
           content="https://opengraph.b-cdn.net/production/images/1171157b-250d-48d1-8b38-936992b37a2d.png?token=a2WslOTnXlU7wkxgcx0HhENUn1vM6WdBYa8CR_L9Q0U&height=949&width=1200&expires=33260249685"
         />
-
-        {/* <title>SK Beauty-Verse</title>
-        <meta
-          name="description"
-          content='I am SUSHMITHA KARTHIK, your dedicated makeup artist, Certified by "Lakme Academy" specializing in hair, skin, and bridal makeup. Discover personalized beauty experiences crafted with premium products, which does not cause damage to skin & hair. "Make your beautyful day, More Beutiful with our Makup Services!"'
-        />
-        <meta property="og:title" content="SK Beauty-Verse" />
-        <meta
-          property="og:description"
-          content='I am SUSHMITHA KARTHIK, your dedicated makeup artist, Certified by "Lakme Academy" specializing in hair, skin, and bridal makeup. Discover personalized beauty experiences crafted with premium products, which does not cause damage to skin & hair. "Make your beautyful day, More Beutiful with our Makup Services!"'
-        />
-        <meta
-          property="og:image"
-          content={"https://www.skbeautyverse.com/images/logo/logo.png"}
-        />
-        <meta property="og:url" content={`https://www.skbeautyverse.com/`} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="SK Beauty-Verse" />
-        <meta
-          name="twitter:description"
-          content='I am SUSHMITHA KARTHIK, your dedicated makeup artist, Certified by "Lakme Academy" specializing in hair, skin, and bridal makeup. Discover personalized beauty experiences crafted with premium products, which does not cause damage to skin & hair. "Make your beautyful day, More Beutiful with our Makup Services!"'
-        />
-        <meta
-          name="twitter:image"
-          content="https://www.skbeautyverse.com/images/logo/logo.png"
-        />
-        <link rel="canonical" href={`https://www.skbeautyverse.com/`} /> */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="canonical" href="https://www.skbeautyverse.com" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content="#317EFB" />
       </Head>
+
       <HomeScreen
         // reviews={homeData.reviews}
         galleryImages={homeData.galleryImages || []}
